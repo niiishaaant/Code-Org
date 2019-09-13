ffe0cdefd10391be2955dd8896e12df12e072df7
Nishant Kedar <nkedar@outlook.com>
2019-09-13 23:12:59 +0530

    Update Homepage
    Removed instance creation from button method to Form area

--- SE Assignment/Homepage.cs
+++ SE Assignment/Homepage.cs
@@ -16,10 +16,10 @@ namespace SE_Assignment
         {
             InitializeComponent();
         }
-
+        SignUp ss = new SignUp();
+        Login mm = new Login();
         private void button1_Click(object sender, EventArgs e)
         {
-            Login mm = new Login();
             mm.Show();
         }
 
@@ -27,10 +27,9 @@ namespace SE_Assignment
         {
 
         }
-
+        
         private void button2_Click_1(object sender, EventArgs e)
-        {
-            SignUp ss = new SignUp();
+        { 
             ss.Show();
         }
     }
