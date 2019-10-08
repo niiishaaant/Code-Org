eaba697cb5da6b596eb405460b5c15e09d87c043
Nishant Kedar <nkedar@outlook.com>
2019-10-08 07:48:10 +0530

    Form Screenshots added

--- SE Assignment/SignUp.Designer.cs
+++ SE Assignment/SignUp.Designer.cs
@@ -232,6 +232,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "SignUp";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SignUp";
             this.Load += new System.EventHandler(this.Signup_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
